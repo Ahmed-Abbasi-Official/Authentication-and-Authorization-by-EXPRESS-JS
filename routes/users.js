6c1dd2329cd9ea039969bf99db7a55586f97867c
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-19 22:53:59 +0500

    Setup Schema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const mongoose=require('mongoose')
+const plm = require('passport-local-mongoose')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect('mongodb://127.0.0.1:27017/Authentication')
 
-module.exports = router;
+const userSchema=mongoose.Schema(
+  {
+    username:{
+      type:String,
+      required:true
+    },
+    name:{
+      type:String,
+      required:true,
+    },
+    password:{
+      type:String,
+      required:true,
+    },
+    secret:String
+  }
+)
+
+userSchema.plugin(plm)
+
+module.exports = mongoose.model("User",userSchema);
