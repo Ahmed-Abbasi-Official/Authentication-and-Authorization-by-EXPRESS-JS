b043f9553f22095a65a15cb26b3697c9d290aeb6
Ahmed0312006 <farhad.khi17@gmail.com>
2024-09-20 10:28:08 +0500

    Setup Auth Logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,97 @@
 var express = require('express');
 var router = express.Router();
+const userModel=require('./users')
+const passport=require('passport')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const localStrategy=require('passport-local')
+
+passport.use(new localStrategy(userModel.authenticate()))
+
+
+router.get('/',(req,res)=>{
+  res.render('index')
+})
+
+
+
+                                // ==========================
+                                //        Profile Route's
+                                // ==========================
+
+
+
+
+router.get('/profile',isLoggedIn,(req,res)=>{
+  res.send('Welcome to Home Page :')
+})
+
+
+
+                                // ==========================
+                                //      Register Route's
+                                // ==========================
+
+
+
+
+router.post('/register',(req,res)=>{
+  var userData=new userModel({
+    username:req.body.username,
+    secret:req.body.secret
+  })
+
+  userModel.register(userData,req.body.password)
+  .then((registereduser)=>{
+    passport.authenticate('local')(req,res,()=>{
+      res.redirect('/profile')
+    })
+  })
+})
+
+
+
+
+                                // ==========================
+                                //        Login Route's
+                                // ==========================
+
+
+
+
+
+router.post('/login',passport.authenticate('local',{
+  successRedirect:'/profile',
+  failureRedirect:'/'
+}),function(req,res){})
+
+
+
+
+                                // ==========================
+                                //        LogOut Route's
+                                // ==========================
+
+
+
+router.get('logout',(req,res,next)=>{
+  req.logout((error)=>{
+    if(error) return next(error) ;
+    res.redirect('/')
+  })
+})
+
+
+                                // ==========================
+                                //        isLogged In
+                                // ==========================
+
+
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next()
+  }
+  res.redirect('/')
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const userSchema=mongoose.Schema(
     },
     name:{
       type:String,
-      required:true,
+      // required:true,
     },
     password:{
       type:String,
